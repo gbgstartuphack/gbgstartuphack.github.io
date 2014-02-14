645880a1fb62745a14e006527199a9a1d08a0fc8
Johan Brook <johan.jvb@gmail.com>
2014-02-14 23:15:30 +0100

    Use 'build' dir for outputting CSS

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ require 'sass-globbing'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "build/css"
 sass_dir = "stylesheets"
 images_dir = "images"
 fonts_dir = "fonts"
